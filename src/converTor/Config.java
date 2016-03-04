5d661ee7344563f1cbbb2bc104812a8f0ac420b8
tom lurge <tomlurge@gmail.com>
2016-03-04 23:11:50 +0100

    moving configuration from Main to seperate class

---
+++ src/converTor/Config.java
@@ -0,0 +1,20 @@
+package converTor;
+
+
+import org.apache.avro.Schema;
+import org.apache.avro.data.Json;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import org.apache.commons.cli.*;  // command line interface
+
+class Config {
+
+  // argument defaults
+
+
+  Config(String[] inputArgs) {
+
+  }
+
+}
